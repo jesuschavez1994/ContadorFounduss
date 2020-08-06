fb70eabe0690cce64eda6416fc98a66042c9df01
jesus Chavez <rc620620@gmail.com>
2020-08-06 17:22:22 -0400

    Nueva visualizacion del contador

--- src/assets/jquery/js/countdown.js
+++ src/assets/jquery/js/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
     year: 2020, // required
-    month: 8, // required
-    day:31, // required
+    month: 10, // required
+    day:6, // required
     hours: 1, // Default is 0 [0-23] integer
     minutes: 14, // Default is 0 [0-59] integer
     seconds: 0, // Default is 0 [0-59] integer
